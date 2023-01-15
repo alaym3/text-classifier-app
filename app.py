b7f798d05787f1959849e321d772016a9e6b3c74
alaym3 <aam5xa@virginia.edu>
2023-01-15 22:36:14 +0100

    fix title

--- app.py
+++ app.py
@@ -3,17 +3,39 @@ import numpy as np
 import pandas as pd
 
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from datasets import load_from_disk
 import torch
-
-# test_dataset = load_from_disk("data/processed/tokenized_test")
+from google.cloud import storage
 
 #### 1st step always: make layout wider #####
-st.set_page_config(layout="wide", page_title="T-Swift")
+st.set_page_config(layout="wide", page_title="text-classification")
 
 st.title('Sentiment classification app')
 st.markdown('### This app allows you to type in a phrase and see how much the phrase is classified to be positive or negative!')
 
+# # load models
+# def read(bucket_name):
+#     """read a blob from GCS using file-like IO"""
+#     # The ID of your GCS bucket
+#     # bucket_name = "your-bucket-name"
+
+#     # The ID of your new GCS object
+#     # blob_name = "storage-object-name"
+
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(bucket_name)
+#     blobs = storage_client.list_blobs(bucket_name)
+
+#     # blob = bucket.blob(blob_name)
+
+#     # Mode can be specified as wb/rb for bytes mode.
+#     # See: https://docs.python.org/3/library/io.html
+#     # with blob.open("w") as f:
+#     #     f.write("Hello world")
+#     for blob in blobs:
+#         with blob.open("r") as f:
+#             st.markdown(f.read())
+
+# read("model-bucket-streamlit-text-classification")
 
 # Load model and tokenizer
 model = AutoModelForSequenceClassification.from_pretrained("models")
