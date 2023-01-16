5778a3a4770798fce41c8166fdd835ddcd9110c0
alaym3 <aam5xa@virginia.edu>
2023-01-16 20:15:07 +0100

    modify prediction scores

--- app.py
+++ app.py
@@ -12,33 +12,9 @@ st.set_page_config(layout="wide", page_title="text-classification")
 st.title('Sentiment classification app')
 st.markdown('### This app allows you to type in a phrase and see how much the phrase is classified to be positive or negative!')
 
-# # load models
-# def read(bucket_name):
-#     """read a blob from GCS using file-like IO"""
-#     # The ID of your GCS bucket
-#     # bucket_name = "your-bucket-name"
-
-#     # The ID of your new GCS object
-#     # blob_name = "storage-object-name"
-
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blobs = storage_client.list_blobs(bucket_name)
-
-#     # blob = bucket.blob(blob_name)
-
-#     # Mode can be specified as wb/rb for bytes mode.
-#     # See: https://docs.python.org/3/library/io.html
-#     # with blob.open("w") as f:
-#     #     f.write("Hello world")
-#     for blob in blobs:
-#         with blob.open("r") as f:
-#             st.markdown(f.read())
-
-# read("model-bucket-streamlit-text-classification")
 
 # Load model and tokenizer
-model = AutoModelForSequenceClassification.from_pretrained("models")
+model = AutoModelForSequenceClassification.from_pretrained("./models")
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 # Prediction for a new phrase
@@ -48,20 +24,16 @@ text = st.text_area('Text to perform sentiment analysis', '''
     ''')
 
 
-
-
 inputs = tokenizer(text, return_tensors="pt") # , padding = True, truncation = True, return_tensors='pt').to('cuda')
 labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
 outputs = model(**inputs, labels=labels)
 predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
 predictions = predictions.cpu().detach().numpy()
-# print('Predictions for the phrase "' + text + '" : ', predictions)
-st.write('Predictions for the phrase "' + text + '" : ', predictions)
 
+# store the positive and negative predictions
+neg_prediction = predictions[0][0]*100
+pos_prediction = predictions[0][1]*100
 
-# if st.checkbox('Show dataframe'):
-#     chart_data = pd.DataFrame(
-#        np.random.randn(20, 3),
-#        columns=['a', 'b', 'c'])
-
-#     chart_data
+# print results
+st.markdown(f'Probability of the phrase being negative: {neg_prediction}%')
+st.markdown(f'Probability of the phrase being positive: {pos_prediction}%')
