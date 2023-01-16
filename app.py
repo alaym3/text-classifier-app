ae08b73ce16de90c5f40264ac34418f3725bf91f
alaym3 <aam5xa@virginia.edu>
2023-01-16 20:44:50 +0100

    Add extra information about the model

--- app.py
+++ app.py
@@ -11,15 +11,23 @@ st.set_page_config(layout="wide", page_title="text-classification")
 
 st.title('Sentiment classification app')
 st.markdown('### This app allows you to type in a phrase and see how much the phrase is classified to be positive or negative!')
-
+st.markdown('### About the model:')
+st.markdown('- We started with the pre-trained transformer [bert-base-uncased](https://huggingface.co/bert-base-uncased) since it is the top used model \
+    for performing Natural Language Processing tasks on English text, including classification and \
+        question-answering. BERT consists of a bidirectional transformer that looks back and forward when \
+            analysing the tokens to learn the context of words.')
+st.markdown('### About the training:')
+st.markdown('- The model has been trained on a [Rotten Tomatoes review dataset](https://huggingface.co/datasets/rotten_tomatoes). The dataset includes two columns: the text from Rotten Tomatoes \
+    reviews for movies, along with a column indicating if the review is positive or negative. [Rotten Tomatoes](https://www.rottentomatoes.com/) is a platform \
+        where movie reviews are submitted by expert audiences and regular people.')
 
 # Load model and tokenizer
-model = AutoModelForSequenceClassification.from_pretrained("./models")
+model = AutoModelForSequenceClassification.from_pretrained("models")
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 # Prediction for a new phrase
 # text = test_dataset[0]['text'] # or text = "I love this movie"
-text = st.text_area('Text to perform sentiment analysis', '''
+text = st.text_area('Write some text to perform sentiment analysis!', '''
     This is a very happy, positive-sounding text sample! Type your own.
     ''')
 
@@ -35,5 +43,5 @@ neg_prediction = predictions[0][0]*100
 pos_prediction = predictions[0][1]*100
 
 # print results
-st.markdown(f'Probability of the phrase being negative: {neg_prediction}%')
-st.markdown(f'Probability of the phrase being positive: {pos_prediction}%')
+st.markdown(f'**Probability of the phrase being negative: {neg_prediction}%**')
+st.markdown(f'**Probability of the phrase being positive: {pos_prediction}%**')
